1f20be46793fe8adada1be089e4a944ec0421524
oldman07 <backagain010@gmail.com>
2021-11-28 08:20:09 +0500

    tutorial 3(activation and deactivation hooks)

--- pwspk_plugin_dev/pwspk.php
+++ pwspk_plugin_dev/pwspk.php
@@ -4,3 +4,18 @@
  * Plugin Name: Pwsdk_plugin
  */
 defined ('ABSPATH')|| die("canot acess directly");
+register_activation_hook(__FILE__,'pwspk_register_activation_hook');
+register_deactivation_hook(__FILE__,'pwspk_register_deactivation_hook');
+register_uninstall_hook( __FILE__,'pwspk_register_uninstall_hook');
+
+
+ function pwspk_register_activation_hook(){
+     add_option('updated_title','this title is hacked');
+}
+
+function pwspk_register_deactivation_hook(){
+    delete_option('updated_title');
+}
+
+
+
