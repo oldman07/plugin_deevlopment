6ddeb8ca53b223f1bda871f391c394f099de227b
oldman07 <backagain010@gmail.com>
2021-11-29 09:32:51 +0500

    tutorial 5(applying filter & adding static resources(css,js))

--- pwspk_plugin_dev/pwspk.php
+++ pwspk_plugin_dev/pwspk.php
@@ -18,3 +18,15 @@ function pwspk_my_shortcode($atts){
      return $atts['message'];
 }
 
+add_filter( 'the_title' , 'pwspk_title' );
+function pwspk_title($title){
+    return "you title is hacked";
+}
+
+add_action( 'wp_enqueue_scripts' ,'pwspk_adding_script');
+
+function pwspk_adding_script(){
+    wp_enqueue_style( 'pwspk_plugin_dev', plugin_dir_url(__FILE__)."assets/css/style.css");
+
+    wp_enqueue_script( 'pwspk_plugin_dev', plugin_dir_url(__FILE__)."assets/js/custom.js",true);
+}
